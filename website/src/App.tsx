2f574ce7fb89aad7136e0812572dd6ed54885e61
esinocchi <evan.sinocchi@gmail.com>
2024-10-15 20:39:35 -0400

    start of front end

---
+++ website/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">LearningMadeEasy</h1>
+        <p className="creators">
+          Created by: <span>Evan Sinocchi, Kasra Ghadimi, Jacob Meert</span>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

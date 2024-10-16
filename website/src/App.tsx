4eea3a8d797adc795c91f4beb7fa1b39e534031d
esinocchi <evan.sinocchi@gmail.com>
2024-10-16 00:59:09 -0400

    front end
    added new features to front page

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const fullText = 'LearningMadeEasy.';
+
+  useEffect(() => {
+    let index = 0;
+    const timer = setInterval(() => {
+      setText(fullText.slice(0, index));
+      index++;
+      if (index > fullText.length) {
+        clearInterval(timer);
+      }
+    }, 100);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="title">LearningMadeEasy</h1>
-        <p className="creators">
-          Created by: <span>Evan Sinocchi, Kasra Ghadimi, Jacob Meert</span>
-        </p>
+        <h1 className="title">{text}</h1>
+        <div className="button-container">
+          <button className="auth-button">Sign Up</button>
+          <button className="auth-button">Log In</button>
+        </div>
       </header>
     </div>
   );
